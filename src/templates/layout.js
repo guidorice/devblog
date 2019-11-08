c7e3f41a4a8cf8d8a1986e0babc32cf665977579
Alex G Rice <rice.guido@gmail.com>
2019-11-08 08:35:38 -0700

    Remove duplicate import of global-styles.

--- src/templates/layout.js
+++ src/templates/layout.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import 'prismjs/themes/prism.css';
 
-import '../global-styles';
 import userConfig from '../../config';
-
 import Header from '../components/Header';
 import GlobalStyle from '../global-styles';
 
