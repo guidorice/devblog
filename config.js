7da7a9bb402105824a2abb5a969c476fbcf1db02
Alex G Rice <rice.guido@gmail.com>
2019-12-19 10:46:19 -0700

    DevSeed post.

--- config.js
+++ config.js
@@ -1,16 +1,15 @@
 module.exports = {
   title: 'Dev Blog',
   author: 'Alex G Rice',
-  description: 'Dev Blog — full stack software engineer with passion for geography and GIS',
+  description: 'Dev Blog of a software engineer with passion for geography and GIS',
   primaryColor: 'rgb(129, 199, 132)',
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5,
   social: {
-    website: 'https://ricegeo.dev',
     github: 'https://github.com/guidorice',
     linkedin: 'https://www.linkedin.com/in/guidorice',
   },
-  pathPrefix: '/devblog',
-  siteUrl: 'https://ricegeo.dev/devblog/',
+  pathPrefix: '/',
+  siteUrl: 'https://blog.ricegeo.dev',
 };
